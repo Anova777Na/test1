1b0911c964036473eab8d0651a234ce0470af1ac
DEEKSHITH-GOWDA-R <deekshithgowda12123@gmail.com>
2025-03-26 12:43:14 +0530

    test1

--- test1.py
+++ test1.py
@@ -1,10 +1,11 @@
 computer_languages = {'C', 'COBOL', 'FOTRAN'}
 languages = {'java', 'ada', 'c#', 'python'}
-computer_languages.add('go') #{'COBOL', 'FOTRAN', 'C', 'go'}
+computer_languages.add('go')
 print(computer_languages)
-computer_languages.update(languages)# {'COBOL', 'ada', 'go', 'C', 'c#', 'FOTRAN', 'java', 'python'
+computer_languages.update(languages)
 print(computer_languages)
 computer_languages.remove('java')
 print(computer_languages)
 computer_languages.clear()
 print(computer_languages)
+ 
